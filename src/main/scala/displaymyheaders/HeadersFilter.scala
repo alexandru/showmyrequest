1605ec95e253def01d2c213c6723453d6b75633c
Alexandru Nedelcu <noreply@alexn.org>
2012-06-21 15:59:43 +0300

    Initial commit

---
+++ src/main/scala/displaymyheaders/HeadersFilter.scala
@@ -0,0 +1,55 @@
+package displaymyheaders
+
+import javax.servlet._
+import http.{HttpServletRequest, HttpServletResponse}
+import java.io._
+import java.util
+import org.yaml.snakeyaml.Yaml
+import collection.JavaConversions._
+
+/**
+ * Author: Alexandru Nedelcu
+ * Email:  contact@alexn.org
+ * Time:   3:26 PM
+ */
+
+class HeadersFilter extends Filter {
+  def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+    val writer = new BufferedWriter(
+      new OutputStreamWriter(response.getOutputStream, "UTF-8"))
+
+    val httpReq = request.asInstanceOf[HttpServletRequest]
+    val httpResp = response.asInstanceOf[HttpServletResponse]
+
+    val resp = new util.LinkedHashMap[String, Any]()
+
+    resp.put("URI", httpReq.getRequestURI)
+    resp.put("Method", httpReq.getMethod)
+    resp.put("Query", Option.apply(httpReq.getQueryString).getOrElse(""))
+    resp.put("Remote-Addr", httpReq.getRemoteAddr)
+    resp.put("Remote-Host", httpReq.getRemoteHost)
+    resp.put("Remote-Port", httpReq.getRemotePort)
+
+    val headers = new util.LinkedHashMap[String, Any]()
+    httpReq.getHeaderNames.foreach { key =>
+      val values = httpReq.getHeaders(key).toArray
+      if (values.length == 1)
+        headers.put(key, values.head)
+      else
+        headers.put(key, values)
+    }
+
+    resp.put("Headers", headers)
+    resp.put("Parameters",  httpReq.getParameterMap)
+
+    httpResp.setStatus(200)
+    httpResp.setContentType("text/plain")
+
+    writer.write(new Yaml().dumpAsMap(resp))
+    writer.close()
+  }
+
+  def init(filterConfig: FilterConfig) {}
+
+  def destroy() {}
+}
---
+++ src/main/scala/displaymyheaders/Server.scala
@@ -0,0 +1,45 @@
+package displaymyheaders
+
+import org.eclipse.jetty.server.{Server => JettyServer}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.eclipse.jetty.util.thread.QueuedThreadPool
+import org.eclipse.jetty.server.nio.SelectChannelConnector
+
+/**
+ * Author: Alexandru Nedelcu
+ * Email:  contact@alexn.org
+ * Time:   3:17 PM
+ */
+
+object Server extends App {
+  val server = new JettyServer()
+
+  val host = "0.0.0.0"
+  val port = if (System.getProperty("PORT") != null)
+    System.getProperty("PORT")
+  else
+    "8080"
+
+  val connector = new SelectChannelConnector()
+  connector.setPort(Integer.valueOf(port))
+  connector.setHost(host)
+  server.addConnector(connector)
+
+  val pool = new QueuedThreadPool()
+  pool.setMinThreads(10)
+  pool.setMaxThreads(100)
+  server.setThreadPool(pool)
+
+  val webXmlPath = this.getClass.getResource("/WEB-INF/web.xml").toExternalForm
+  val resourceBase = this.getClass.getResource("/").toExternalForm
+
+  val context = new WebAppContext()
+  context.setDescriptor(webXmlPath)
+  context.setResourceBase(resourceBase)
+  context.setContextPath("/")
+  context.setParentLoaderPriority(true)
+
+  server.setHandler(context)
+  server.start()
+  server.join()
+}
