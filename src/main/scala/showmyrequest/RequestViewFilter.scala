04189906e8640e128f9dad832691b431dce7527a
Alexandru Nedelcu <noreply@alexn.org>
2012-06-22 17:41:41 +0300

    Small fix

--- src/main/scala/showmyrequest/RequestViewFilter.scala
+++ src/main/scala/showmyrequest/RequestViewFilter.scala
@@ -15,51 +15,55 @@ import collection.JavaConversions._
 
 class RequestViewFilter(locator: GeoIPLocator) extends Filter with Metrics {
   def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-    timer.time {
-      val out = response.getOutputStream
-      val writer = new BufferedWriter(
-        new OutputStreamWriter(out, "UTF-8"))
+    val httpReq = request.asInstanceOf[HttpServletRequest]
+    if (httpReq.getPathInfo.startsWith("/admin"))
+      chain.doFilter(request, response)
+    else
+      timer.time {
+        val out = response.getOutputStream
+        val writer = new BufferedWriter(
+          new OutputStreamWriter(out, "UTF-8"))
 
-      val httpReq = request.asInstanceOf[HttpServletRequest]
-      val httpResp = response.asInstanceOf[HttpServletResponse]
+        val httpResp = response.asInstanceOf[HttpServletResponse]
 
-      val resp = new util.LinkedHashMap[String, Any]()
+        val resp = new util.LinkedHashMap[String, Any]()
 
-      resp.put("Method", httpReq.getMethod)
-      resp.put("Remote-Addr", httpReq.getRemoteAddr)
-      resp.put("Remote-Port", httpReq.getRemotePort)
+        resp.put("Method", httpReq.getMethod)
+        resp.put("Path", httpReq.getPathInfo)
+        resp.put("Remote-Addr", httpReq.getRemoteAddr)
+        resp.put("Remote-Port", httpReq.getRemotePort)
 
-      val headers = new util.LinkedHashMap[String, Any]()
-      httpReq.getHeaderNames.filterNot(_.toLowerCase.contains("heroku")).
-        filterNot(_ == "X-Request-Start").
-        foreach {
-        key =>
-          val values = httpReq.getHeaders(key).toArray
-          if (values.length == 1)
-            headers.put(key, values.head)
-          else
-            headers.put(key, values)
+        val headers = new util.LinkedHashMap[String, Any]()
+        httpReq.getHeaderNames.filterNot(_.toLowerCase.contains("heroku")).
+          filterNot(_ == "X-Request-Start").
+          foreach {
+          key =>
+            val values = httpReq.getHeaders(key).toArray
+            if (values.length == 1)
+              headers.put(key, values.head)
+            else
+              headers.put(key, values)
+        }
+
+        val forwardedKey = headers.keySet().find(_.toLowerCase == "x-forwarded-for")
+        val realIP = forwardedKey match {
+          case Some(v) => headers.get(v).asInstanceOf[String].split(',')(0).stripMargin
+          case None => httpReq.getRemoteAddr
+        }
+
+        resp.put("Real-IP", realIP)
+        resp.put("GeoIP-Location", locator.locate(realIP))
+
+        resp.put("Headers", headers)
+        resp.put("Parameters", httpReq.getParameterMap)
+
+        httpResp.setStatus(200)
+        httpResp.setContentType("text/plain")
+
+        writer.write(new Yaml().dumpAsMap(resp))
+        writer.close()
+        out.close()
       }
-
-      val forwardedKey = headers.keySet().find(_.toLowerCase == "x-forwarded-for")
-      val realIP = forwardedKey match {
-        case Some(v) => headers.get(v).asInstanceOf[String].split(',')(0).stripMargin
-        case None => httpReq.getRemoteAddr
-      }
-
-      resp.put("Real-IP", realIP)
-      resp.put("GeoIP-Location", locator.locate(realIP))
-
-      resp.put("Headers", headers)
-      resp.put("Parameters", httpReq.getParameterMap)
-
-      httpResp.setStatus(200)
-      httpResp.setContentType("text/plain")
-
-      writer.write(new Yaml().dumpAsMap(resp))
-      writer.close()
-      out.close()
-    }
   }
 
   def init(filterConfig: FilterConfig) {}
--- src/main/scala/showmyrequest/Service.scala
+++ src/main/scala/showmyrequest/Service.scala
@@ -12,7 +12,7 @@ import com.yammer.dropwizard.config.Environment
 object Service extends ScalaService[Configuration]("showmyrequest") {
   def initialize(conf: Configuration, env: Environment) {
     val geoip = new GeoIPLocator(conf.geoipDBPath)
-    env.addFilter(new RequestViewFilter(geoip), "/")
+    env.addFilter(new RequestViewFilter(geoip), "/*")
     env.addResource(new RootResource)
     env.addHealthCheck(new GeoIPHealthCheck(conf.geoipDBPath))
   }
